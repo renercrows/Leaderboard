ab7834c06ba2d6d1e765cfca3d9afa74ba4aa231
renercrows <ungredda95@gmail.com>
2022-07-29 00:13:01 -0400

    Fix JS

--- src/main.js
+++ src/main.js
@@ -105,7 +105,6 @@ addBtn.addEventListener('click', () => {
 /* eslint-disable no-undef */
 const listLink = document.getElementById('list-btn');
 const addNewLink = document.getElementById('add-new-btn');
-const invitation = document.getElementById('invitation');
 const contactLink = document.getElementById('contact-btn');
 
 const list = document.getElementById('list');
@@ -115,42 +114,3 @@ let visibility = 'list';
 if (localStorage.getItem('visibility')) {
   visibility = localStorage.getItem('visibility');
 }
-
-const toggleList = () => {
-  if (list.classList.contains('invisible')) {
-    list.classList.toggle('invisible');
-    addNew.classList.add('invisible');
-    contact.classList.add('invisible');
-  }
-  visibility = 'list';
-  localStorage.setItem('visibility', visibility);
-  listLink.style.color = 'gray';
-  addNewLink.style.color = 'black';
-  contactLink.style.color = 'black';
-};
-
-const toggleAddNew = () => {
-  if (addNew.classList.contains('invisible')) {
-    addNew.classList.toggle('invisible');
-    list.classList.add('invisible');
-    contact.classList.add('invisible');
-  }
-  visibility = 'add-new';
-  localStorage.setItem('visibility', visibility);
-  addNewLink.style.color = 'gray';
-  listLink.style.color = 'black';
-  contactLink.style.color = 'black';
-};
-
-const toggleContact = () => {
-  if (contact.classList.contains('invisible')) {
-    contact.classList.toggle('invisible');
-    list.classList.add('invisible');
-    addNew.classList.add('invisible');
-  }
-  visibility = 'contact';
-  localStorage.setItem('visibility', visibility);
-  contactLink.style.color = 'gray';
-  listLink.style.color = 'black';
-  addNewLink.style.color = 'black';
-};
